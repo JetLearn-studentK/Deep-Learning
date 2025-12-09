447770ac239e88a33d3dd4ba08f9cdba9f84d153
JetLearn_studentK <gellardkai771@gmail.com>
2025-12-09 16:57:19 +0000

    Add files via upload

--- fingerDigits.py
+++ fingerDigits.py
@@ -7,20 +7,31 @@ import matplotlib.pyplot as plt
 
 Sequential = load_model(r"C:\Users\kaige\python projects\Deep Learning\Sequential.h5")
 prediction = 0
+number = 0
+blankImage = 0
 
 
 screen = tk.Tk()
 screen.title("DL Digits")
 screen.geometry("500x500")
+canvas = tk.Canvas(screen, bg = "white", height = 250, width = 250)
+canvas.place(x = 120, y = 100)
 
 
 def mouseDrawing(event):
     canvas.create_oval(event.x, event.y, event.x + 5, event.y + 5, fill = "green", outline = "#1c6537")
 
+#img = canvas.postscript(file="temp_canvas.ps")  # Save canvas content as .ps (PostScript), to make it not based on vectors.
+#blankImage = Image.open("temp_canvas.ps")
 blankImage = Image.new("L", (250, 250), color = "white")
 number = ImageDraw.Draw(blankImage)
 
-# 
+def Clear():
+    global number, blankImage
+    canvas.delete("All")
+    number = 0
+    blankImage = 0
+
 
 
 def Predict():
@@ -51,7 +62,7 @@ button2 = tk.Button(screen, text="Clear",
                     activebackground="red",
                     activeforeground="black",
                     width=10,
-                    command=lambda: canvas.delete("all"),
+                    command= lambda: Clear(),
                     cursor="hand2")
 button2.place(x=330, y=430)
 
@@ -61,8 +72,7 @@ label.place(x = 160, y = 0)
 label2 = tk.Label(screen, text = "", font = ("arial", 10), bg = "green", fg = "white", bd = 2)
 label2.place(x = 360, y = 480)
 
-canvas = tk.Canvas(screen, bg = "white", height = 250, width = 250)
-canvas.place(x = 120, y = 100)
+
 
 
 
