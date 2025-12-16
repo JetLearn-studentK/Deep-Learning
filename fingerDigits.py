61f1fc7ba90eedec70264b5fe1d9f2bec61ae9ad
JetLearn_studentK <gellardkai771@gmail.com>
2025-12-16 17:59:52 +0000

    Add files via upload

--- fingerDigits.py
+++ fingerDigits.py
@@ -7,8 +7,7 @@ import matplotlib.pyplot as plt
 
 Sequential = load_model(r"C:\Users\kaige\python projects\Deep Learning\Sequential.h5")
 prediction = 0
-number = 0
-blankImage = 0
+
 
 
 screen = tk.Tk()
@@ -20,19 +19,35 @@ label2 = tk.Label(screen, text = "", font = ("arial", 10), bg = "green", fg = "w
 label2.place(x = 360, y = 480)
 
 
+
+
 def mouseDrawing(event):
-    canvas.create_oval(event.x, event.y, event.x + 5, event.y + 5, fill = "green", outline = "#1c6537")
+    #canvas.create_oval(event.x, event.y, event.x + 5, event.y + 5, fill = "green", outline = "#1c6537")
+    global isEraser, scrollSize
+    lastX, lastY = event.x, event.y
+
+    canvas.create_line(lastX,
+        lastY,
+        event.x,
+        event.y,
+        width=5,
+        fill="#1c6537",
+        capstyle=tk.ROUND,
+        smooth=True)
+
+    #number.ellipse((event.x, event.y, event.x + 5, event.y + 5), "#1c6537")
+    number.line((lastX, lastY, event.x, event.y), "#1c6537", 5)
 
 #img = canvas.postscript(file="temp_canvas.ps")  # Save canvas content as .ps (PostScript), to make it not based on vectors.
 #blankImage = Image.open("temp_canvas.ps")
-blankImage = Image.new("L", (250, 250), color = "white")
-number = ImageDraw.Draw(blankImage)
+blankImage = Image.new(mode = "L", size = (250, 250), color = "black")
+number = ImageDraw.Draw(im = blankImage)
 
 def Clear():
     global number, blankImage
     canvas.delete("all")
-    blankImage = Image.new("L", (250, 250), color = "white")
-    number = ImageDraw.Draw(blankImage)
+    blankImage = Image.new(mode = "L", size = (250, 250), color = "black")
+    number = ImageDraw.Draw(im = blankImage)
     label2.config(text = "")
 
 
@@ -40,19 +55,18 @@ def Clear():
 def Predict():
     global prediction
     proccessedImage = blankImage.resize((28, 28))
-    print(proccessedImage)
     #plt.imshow(proccessedImage)
     proccessedImage = np.array(proccessedImage)
-    plt.imshow(proccessedImage)
-    plt.show()
+    proccessedImage = 255 - proccessedImage
     proccessedImage = proccessedImage / 255.0
     proccessedImage = np.expand_dims(proccessedImage, axis = 0)
-    #plt.imshow(proccessedImage)
+    plt.imshow(proccessedImage[0])
+    plt.show()
     #print(proccessedImage)
     prediction = Sequential.predict(proccessedImage)
     print(prediction)
     prediction = np.argmax(prediction)
-    label2.config(text = prediction)
+    label2.config(text = str(prediction))
     #print(prediction)
 
 
@@ -83,6 +97,8 @@ label.place(x = 160, y = 0)
 
 
 canvas.bind("<B1-Motion>", mouseDrawing)
+
+
 
 
 
