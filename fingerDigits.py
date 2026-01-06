290e90c11a6218c050ceef66494c2208e791e7dc
JetLearn_studentK <gellardkai771@gmail.com>
2026-01-06 18:02:10 +0000

    Add files via upload

--- fingerDigits.py
+++ fingerDigits.py
@@ -22,31 +22,32 @@ label2.place(x = 360, y = 480)
 
 
 def mouseDrawing(event):
-    #canvas.create_oval(event.x, event.y, event.x + 5, event.y + 5, fill = "green", outline = "#1c6537")
+    #canvas.create_oval(event.x, event.y, event.x + 5, event.y + 5, fill = "#000000")
     global isEraser, scrollSize
     lastX, lastY = event.x, event.y
-
+    canvas.create_oval(lastX, lastY, lastX + 5, lastY + 5, fill = "#000000")
+    '''
     canvas.create_line(lastX,
         lastY,
-        event.x,
-        event.y,
+        lastX + 5,
+        lastY + 5,
         width=5,
-        fill="#1c6537",
+        fill="#000000",
         capstyle=tk.ROUND,
         smooth=True)
-
-    #number.ellipse((event.x, event.y, event.x + 5, event.y + 5), "#1c6537")
-    number.line((lastX, lastY, event.x, event.y), "#1c6537", 5)
+    '''
+    number.ellipse((event.x, event.y, event.x + 5, event.y + 5), "#000000")
+    #number.line((lastX, lastY, lastX + 5, lastY + 5), fill = "#000000")
 
 #img = canvas.postscript(file="temp_canvas.ps")  # Save canvas content as .ps (PostScript), to make it not based on vectors.
 #blankImage = Image.open("temp_canvas.ps")
-blankImage = Image.new(mode = "L", size = (250, 250), color = "black")
+blankImage = Image.new(mode = "L", size = (250, 250), color = "white")
 number = ImageDraw.Draw(im = blankImage)
 
 def Clear():
     global number, blankImage
     canvas.delete("all")
-    blankImage = Image.new(mode = "L", size = (250, 250), color = "black")
+    blankImage = Image.new(mode = "L", size = (250, 250), color = "white")
     number = ImageDraw.Draw(im = blankImage)
     label2.config(text = "")
 
@@ -54,12 +55,13 @@ def Clear():
 
 def Predict():
     global prediction
-    proccessedImage = blankImage.resize((28, 28))
+    proccessedImage = blankImage.resize((28, 28)).convert(mode = "L")
     #plt.imshow(proccessedImage)
     proccessedImage = np.array(proccessedImage)
     proccessedImage = 255 - proccessedImage
     proccessedImage = proccessedImage / 255.0
     proccessedImage = np.expand_dims(proccessedImage, axis = 0)
+    proccessedImage = np.expand_dims(proccessedImage, axis = 3)
     plt.imshow(proccessedImage[0])
     plt.show()
     #print(proccessedImage)
